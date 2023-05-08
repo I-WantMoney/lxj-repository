746c199a984760ce7bc3c6508b5ae24889c7485a
lixingjian@rayoo.jp <lixingjian@rayoo.jp>
2023-05-08 10:31:32 +0900

    Add AI function

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -15,5 +15,29 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "predictions": {
+    "identifyLabelsebddad5b": {
+      "region": "string",
+      "type": "string"
+    },
+    "identifyText832e7774": {
+      "format": "string",
+      "region": "string"
+    },
+    "interpretText5fdabf67": {
+      "region": "string",
+      "type": "string"
+    },
+    "speechGenerator6caebe87": {
+      "language": "string",
+      "region": "string",
+      "voice": "string"
+    },
+    "translateText719c57ac": {
+      "region": "string",
+      "sourceLang": "string",
+      "targetLang": "string"
+    }
   }
 }
---
+++ src/components/IdentifyText.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="left">
+      <div class="imageUploader">
+        <input type="file" @change="identifyFromFile" accept="image/jpeg, image/png" /><br />
+        <img v-bind:width="width" v-if="uploadedImage" v-bind:src="uploadedImage" />
+      </div>
+    </div>
+    <div class="right">
+      <textarea v-model="response" class="ioarea" disabled></textarea>
+    </div>
+  </template>
+  
+  <script>
+  import { Predictions } from 'aws-amplify';
+  import { ref } from 'vue';
+  
+  export default {
+    setup() {
+      const uploadedImage = ref(null);
+      const response = ref('');
+  
+      const identifyFromFile = (e) => {
+        const file = e.target.files || e.dataTranfer.files;
+        if (!file) return;
+  
+        createImage(file[0]);
+        // Identify Text の実装
+        Predictions.identify({
+          text: {
+            source: {
+              file: file[0],
+            },
+            type: 'ALL',
+          },
+        })
+          .then((result) => {
+            response.value = JSON.stringify(result, null, '\t');
+          })
+            .catch((error) => console.warn(error));
+
+        // ↑↑↑↑↑↑
+      };
+  
+      const createImage = (file) => {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          uploadedImage.value = e.target.result;
+        };
+        reader.readAsDataURL(file);
+      };
+  
+      return {
+        uploadedImage,
+        response,
+        identifyFromFile,
+      };
+    },
+  };
+  </script>
+  
